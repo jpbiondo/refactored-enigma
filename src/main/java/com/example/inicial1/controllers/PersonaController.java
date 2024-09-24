ef56d90f3012ebe509cb60c918927842ae618ca3
Juan Pablo <jupbiondo@gmail.com>
2024-09-24 08:40:48 -0300

    Hacer abstracta BaseServiceImpl

--- src/main/java/com/example/inicial1/controllers/PersonaController.java
+++ src/main/java/com/example/inicial1/controllers/PersonaController.java
@@ -10,53 +10,5 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/personas")
 public class PersonaController extends BaseControllerImpl {
-    private PersonaServiceImpl personaServiceImpl;
-    public PersonaController(PersonaServiceImpl personaServiceImpl) {
-        this.personaServiceImpl = personaServiceImpl;
-    }
 
-    @GetMapping("")
-    public ResponseEntity<?> getAll() {
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaServiceImpl.findAll());
-        } catch(Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("\"error\":\"Error, por favor intente más tarde.\"}");
-        }
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getOne(@PathVariable Long id) {
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaServiceImpl.findById(id));
-        } catch(Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("\"error\":\"Error, por favor intente más tarde.\"}");
-        }
-    }
-
-    @PostMapping("")
-    public ResponseEntity<?> save(@RequestBody Persona persona) {
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaServiceImpl.save(persona));
-        } catch(Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("\"error\":\"Error, por favor intente más tarde.\"}");
-        }
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Persona persona) {
-        try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaServiceImpl.update(id, persona));
-        } catch(Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("\"error\":\"Error, por favor intente más tarde.\"}");
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
-        try {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(personaServiceImpl.delete(id));
-        } catch(Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("\"error\":\"Error, por favor intente más tarde.\"}");
-        }
-    }
 }
--- src/main/java/com/example/inicial1/services/BaseServiceImpl.java
+++ src/main/java/com/example/inicial1/services/BaseServiceImpl.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
-public class BaseServiceImpl<E extends Base, ID extends Serializable> implements BaseService<E, ID> {
+public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> implements BaseService<E, ID> {
     protected BaseRepository<E, ID> baseRepository;
     public BaseServiceImpl(BaseRepository<E,ID> baseRepository) {
         this.baseRepository = baseRepository;
