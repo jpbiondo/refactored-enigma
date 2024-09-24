1ded16ee10b76fa15a4939bdcab0ccb11716c6d5
Juan Pablo <jupbiondo@gmail.com>
2024-09-23 21:27:46 -0300

    Añadir implementaciones de intarfaces de servicios

--- src/main/java/com/example/inicial1/services/AutorServiceImpl.java
+++ src/main/java/com/example/inicial1/services/AutorServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AutorServiceImpl extends BaseServiceImpl<Autor, Long> {
+public class AutorServiceImpl extends BaseServiceImpl<Autor, Long> implements AutorService {
     @Autowired
     private AutorRepository autorRepository;
 
--- src/main/java/com/example/inicial1/services/PersonaServiceImpl.java
+++ src/main/java/com/example/inicial1/services/PersonaServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PersonaServiceImpl extends BaseServiceImpl {
+public class PersonaServiceImpl extends BaseServiceImpl<Persona, Long> implements PersonaService {
     @Autowired
     private PersonaRepository personaRepository;
 
