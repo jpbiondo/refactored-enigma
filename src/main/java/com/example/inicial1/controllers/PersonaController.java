3d6bfa02f270ba71982e7288eb4584fb5f85cf7d
Juan Pablo <jupbiondo@gmail.com>
2024-09-23 21:24:16 -0300

    Refactor: renombrar personaservice a personaserviceImpl

--- src/main/java/com/example/inicial1/controllers/PersonaController.java
+++ src/main/java/com/example/inicial1/controllers/PersonaController.java
@@ -1,7 +1,7 @@
 package com.example.inicial1.controllers;
 
 import com.example.inicial1.entities.Persona;
-import com.example.inicial1.services.PersonaService;
+import com.example.inicial1.services.PersonaServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,15 +10,15 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/personas")
 public class PersonaController {
-    private PersonaService personaService;
-    public PersonaController(PersonaService personaService) {
-        this.personaService = personaService;
+    private PersonaServiceImpl personaServiceImpl;
+    public PersonaController(PersonaServiceImpl personaServiceImpl) {
+        this.personaServiceImpl = personaServiceImpl;
     }
 
     @GetMapping("")
     public ResponseEntity<?> getAll() {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaService.findAll());
+            return ResponseEntity.status(HttpStatus.OK).body(personaServiceImpl.findAll());
         } catch(Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("\"error\":\"Error, por favor intente más tarde.\"}");
         }
@@ -27,7 +27,7 @@ public class PersonaController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getOne(@PathVariable Long id) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaService.findById(id));
+            return ResponseEntity.status(HttpStatus.OK).body(personaServiceImpl.findById(id));
         } catch(Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("\"error\":\"Error, por favor intente más tarde.\"}");
         }
@@ -36,7 +36,7 @@ public class PersonaController {
     @PostMapping("")
     public ResponseEntity<?> save(@RequestBody Persona persona) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaService.save(persona));
+            return ResponseEntity.status(HttpStatus.OK).body(personaServiceImpl.save(persona));
         } catch(Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("\"error\":\"Error, por favor intente más tarde.\"}");
         }
@@ -45,7 +45,7 @@ public class PersonaController {
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Persona persona) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(personaService.update(id, persona));
+            return ResponseEntity.status(HttpStatus.OK).body(personaServiceImpl.update(id, persona));
         } catch(Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("\"error\":\"Error, por favor intente más tarde.\"}");
         }
@@ -54,7 +54,7 @@ public class PersonaController {
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         try {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(personaService.delete(id));
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(personaServiceImpl.delete(id));
         } catch(Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("\"error\":\"Error, por favor intente más tarde.\"}");
         }
--- src/main/java/com/example/inicial1/services/PersonaServiceImpl.java
+++ src/main/java/com/example/inicial1/services/PersonaServiceImpl.java
@@ -3,20 +3,16 @@ package com.example.inicial1.services;
 import com.example.inicial1.entities.Persona;
 import com.example.inicial1.repositories.BaseRepository;
 import com.example.inicial1.repositories.PersonaRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
-public class PersonaService extends BaseServiceImpl {
+public class PersonaServiceImpl extends BaseServiceImpl {
     @Autowired
     private PersonaRepository personaRepository;
 
 
-    public PersonaService(BaseRepository<Persona, Long> baseRepository){
+    public PersonaServiceImpl(BaseRepository<Persona, Long> baseRepository){
         super(baseRepository);
     }
 
