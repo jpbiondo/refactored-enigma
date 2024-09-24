11aa281361df70f8ee13526496734300fb49ab62
Juan Pablo <jupbiondo@gmail.com>
2024-09-23 21:19:46 -0300

    Añadir herencia de PersonaService al padre BaseSeriveImpl

--- src/main/java/com/example/inicial1/services/PersonaService.java
+++ src/main/java/com/example/inicial1/services/PersonaService.java
@@ -9,72 +9,13 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class PersonaService implements BaseService<Persona, Long> {
+public class PersonaService extends BaseServiceImpl {
     //@Autowired tambien permite la inyeccion de dependencia
     private PersonaRepository personaRepository;
 
 
     public PersonaService(PersonaRepository personaRepository){
-        this.personaRepository = personaRepository;
+        super(personaRepository);
     }
 
-    @Override
-    @Transactional
-    public List<Persona> findAll() throws Exception {
-        try{
-            return personaRepository.findAll();
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    @Override
-    @Transactional
-    public Persona findById(Long id) throws Exception {
-        try{
-            Optional<Persona> personaOptional = personaRepository.findById(id);
-            return personaOptional.get();
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    @Override
-    @Transactional
-    public Persona save(Persona entity) throws Exception {
-        try{
-            return personaRepository.save(entity);
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    @Override
-    @Transactional
-    public Persona update(Long id, Persona entity) throws Exception {
-        try {
-            Optional<Persona> personaOptional = personaRepository.findById(id);
-            Persona persona = personaOptional.get();
-            return personaRepository.save(entity);
-        } catch(Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    @Override
-    @Transactional
-    public boolean delete(Long id) throws Exception {
-        try {
-            if(personaRepository.existsById(id)) {
-                personaRepository.deleteById(id);
-                return true;
-            }
-
-            throw new Exception();
-
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-
-    }
 }
