98b58b248b190eee6a383229ec1ac7c0a3b501f7
Juan Pablo <jupbiondo@gmail.com>
2024-09-25 11:29:15 -0300

    Create service and controller methods for search request

--- src/main/java/com/example/inicial1/services/PersonaService.java
+++ src/main/java/com/example/inicial1/services/PersonaService.java
@@ -3,5 +3,8 @@ package com.example.inicial1.services;
 
 import com.example.inicial1.entities.Persona;
 
+import java.util.List;
+
 public interface PersonaService extends BaseService<Persona, Long> {
+    List<Persona> search(String filtro) throws Exception;
 }
--- src/main/java/com/example/inicial1/services/PersonaServiceImpl.java
+++ src/main/java/com/example/inicial1/services/PersonaServiceImpl.java
@@ -6,14 +6,25 @@ import com.example.inicial1.repositories.PersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PersonaServiceImpl extends BaseServiceImpl<Persona, Long> implements PersonaService {
     @Autowired
     private PersonaRepository personaRepository;
 
 
-    public PersonaServiceImpl(BaseRepository<Persona, Long> baseRepository){
+    public PersonaServiceImpl(BaseRepository<Persona, Long> baseRepository) {
         super(baseRepository);
     }
 
+    @Override
+    public List<Persona> search(String filtro) throws Exception {
+        try {
+            return this.personaRepository.findByNombreContainingOrApellidoContaining(filtro, filtro);
+
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
 }
